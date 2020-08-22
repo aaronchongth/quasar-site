3e14b8b00323efae0d08502a6365737adf33d68d
Aaron Chong <aaronchongth@gmail.com>
2020-08-22 17:16:55 +0800

    Initial reading list added

--- src/pages/AboutMe.vue
+++ src/pages/AboutMe.vue
@@ -18,6 +18,55 @@
         In my free time, I like to build and learn new things, read books, or play volleyball.</br>
         Right now, I am having fun with the <span><a href="https://quasar.dev/">Quasar Framework</a></span>.
       </div>
+
+      <div 
+        class="row inline">
+
+        <div class="q-pa-md column inline" style="max-width: 300px">
+          <q-list bordered padding>
+            <q-item>
+              <q-item-section>
+                <q-item-label overline>READING LIST</q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-separator />
+
+            <q-item
+              v-for="(book, $index) in current_reading"
+              :key="$index"
+            >
+              <q-item-section>
+                {{ book.name }}
+              </q-item-section>
+              <q-item-section style="max-width: 150px">
+                <q-item-label caption>
+                  {{ book.author }}
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-separator />
+
+            <q-item
+              v-for="(book, $index) in books"
+              :key="$index"
+            >
+              <q-item-section>
+                {{ book.name }}
+              </q-item-section>
+              <q-item-section style="max-width: 150px">
+                <q-item-label caption>
+                  {{ book.author }}
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+
+          </q-list>
+        </div>
+
+      </div>
+      
     </div>
 
   </q-page>
@@ -30,7 +79,39 @@ export default {
     return {
       variable_stiffness_expanded: false,
       stiffness_modulator_expanded: false,
-      analytical_design_expanded: false
+      analytical_design_expanded: false,
+      current_reading:  [
+        {
+          name: 'The Alchemist',
+          author: 'Paulo Coelho'
+        },
+        {
+          name: '12 Rules for Life',
+          author: 'Jordan B. Peterson'
+        }
+      ],
+      books: [
+        {
+          name: 'Crime and Punishment',
+          author: 'Fyodor Dostoevsky'
+        },
+        {
+          name: 'Rich Dad Poor Dad',
+          author: 'Robert Kiyosaki'
+        },
+        {
+          name: 'The Gulag Archipelago',
+          author: 'Aleksandr Solzhenitsyn'
+        },
+        {
+          name: 'The Book of Joy',
+          author: 'Desmond Tutu, 14th Dalai Lama'
+        },
+        {
+          name: '5 Love Languages',
+          author: 'Gary Chapman'
+        }
+      ]
     }
   },
   components: {
